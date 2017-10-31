65718aa59c8591b86358ae3368d73cdbe77e3e31
Mohamed KRISTOU <krisstwo@gmail.com>
2017-10-31 18:57:02 +0100

    #7 Customer receives email on reservation refusal

--- src/Coffeeandbrackets/UniqueCodeBundle/Controller/DefaultController.php
+++ src/Coffeeandbrackets/UniqueCodeBundle/Controller/DefaultController.php
@@ -193,6 +193,18 @@ class DefaultController extends Controller
                 $reservationService = $this->get('unique_code.reservation');
                 $reservationService->hotelRefuseReservation($reservation,$data);
 
+                $serviceMail = $this->container->get('unique_code.mailer');
+                $tabParam = array(
+                    'to' => $reservation->getCustomer()->getEmail(),
+                    'template' => 'UniqueCodeBundle:Email:customer-reservation-refused.html.twig',
+                    'subject' => 'Demande de réservation refusée',
+                    'from' => array($this->container->getParameter('mailer_user') => 'HappyBreak'),
+                    'params' => array(
+                        'reservation' => $reservation
+                    )
+                );
+                $serviceMail->sendMessage($tabParam, 'text/html');
+
                 return new JsonResponse(array());
             }else {
                 return new JsonResponse(array('error' => 'Données invalides'));
