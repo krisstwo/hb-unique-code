2d3eb409b5b450c6346213fe71c9fc97b4b2a8d9
Mohamed KRISTOU <krisstwo@gmail.com>
2017-11-21 09:50:59 +0100

    #3 Fixed for some form errors

--- src/Coffeeandbrackets/UniqueCodeBundle/Form/CreateReservation.php
+++ src/Coffeeandbrackets/UniqueCodeBundle/Form/CreateReservation.php
@@ -8,10 +8,10 @@ namespace Coffeeandbrackets\UniqueCodeBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\DateTime;
-use Symfony\Component\Validator\Constraints\IdenticalTo;
 use Symfony\Component\Validator\Constraints\Length;
 
 class CreateReservation extends AbstractType
@@ -23,7 +23,7 @@ class CreateReservation extends AbstractType
             ->add('last_name', null, array('required' => true))
             ->add('first_name', null, array('required' => true))
             ->add('email', EmailType::class, array('required' => true))
-            ->add('re_email', EmailType::class, array('required' => true, 'constraints' => array(new IdenticalTo(array('value' => 'email')))))
+            ->add('re_email', EmailType::class, array('required' => true))
             ->add('phone', null, array('required' => true))
             ->add('cgv', null, array('required' => true))
             ->add('number_person', null, array('required' => true))
@@ -32,7 +32,7 @@ class CreateReservation extends AbstractType
             ->add('date', null, array('required' => true, 'constraints' => array(new DateTime(array('format' => 'd/m/Y')))))
             ->add('number_night', null, array('required' => true))
             ->add('offer', null, array('required' => true))
-            ->add('customer_msg', null, array('constraints' => array(new Length(array('max' => 255)))));
+            ->add('customer_msg', TextareaType::class, array('empty_data' => '', 'constraints' => array(new Length(array('max' => 255)))));
     }
 
     public function configureOptions(OptionsResolver $resolver)
