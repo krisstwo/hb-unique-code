8537c84eac81ed5d73d984a5f8f28e57f9d5d4d1
Mohamed KRISTOU <krisstwo@gmail.com>
2017-11-21 11:24:08 +0100

    Step 2 validatition now re-checks for code validity

--- src/Coffeeandbrackets/UniqueCodeBundle/Form/CreateCustomer.php
+++ src/Coffeeandbrackets/UniqueCodeBundle/Form/CreateCustomer.php
@@ -6,27 +6,56 @@
 
 namespace Coffeeandbrackets\UniqueCodeBundle\Form;
 
+use Coffeeandbrackets\UniqueCodeBundle\Service\CheckCode;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Callback;
 use Symfony\Component\Validator\Constraints\DateTime;
 use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 class CreateCustomer extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $codeCallback = function ($code, ExecutionContextInterface $context, $payload) use ($options) {
+            /**
+             * @var $checker CheckCode
+             */
+            $checker = $options['code_check'];
+
+            switch ($checker->validate($code)) {
+                case CheckCode::INVALID_CODE_NOT_FOUND:
+                    $context->buildViolation('Le code unique indiqué n\'est pas valide.')
+                            ->atPath('firstName')
+                            ->addViolation();
+                    break;
+                case CheckCode::INVALID_CODE_USED:
+                    $context->buildViolation('Le code unique indiqué a déjà été utilisé.')
+                            ->atPath('firstName')
+                            ->addViolation();
+                    break;
+                case CheckCode::INVALID_CODE_RESERVED:
+                    $context->buildViolation('Le code unique indiqué a déjà une demande de reservation en cours. Vous ne pouvez envoyer plusieurs demandes de réservation en même temps.')
+                            ->atPath('firstName')
+                            ->addViolation();
+                    break;
+                default:
+                    break;
+            }
+        };
+
         $builder
-            ->add('code', null, array('required' => true))
+            ->add('code', null, array('required' => true, 'constraints' => array(new Callback($codeCallback))))
             ->add('last_name', null, array('required' => true))
             ->add('first_name', null, array('required' => true))
             ->add('email', EmailType::class, array('required' => true))
             ->add('re_email', EmailType::class, array('required' => true))
             ->add('phone', null, array('required' => true))
-            ->add('cgv', null, array('required' => true))
-            ;
+            ->add('cgv', null, array('required' => true));
     }
 
     public function configureOptions(OptionsResolver $resolver)
