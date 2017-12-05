778777f93e331a25aed00292cb30d8763cf49022
Med Hedi JEBALI <medhedijebali@bitbucket.org>
2017-12-05 15:03:23 +0100

    [RECETTE_3] commande proposition non vue apres 72h

---
+++ src/Coffeeandbrackets/UniqueCodeBundle/Command/CodeUniqueHandleUnansweredHotelPropositionCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Coffeeandbrackets\UniqueCodeBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class CodeUniqueHandleUnansweredHotelPropositionCommand extends ContainerAwareCommand {
+    protected function configure() {
+        $this
+            ->setName('code-unique:handle-unanswered-hotel-proposition')
+            ->setDescription('Decline hotel proposition automatically after 72h');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output) {
+        $unansweredHotelPropositions = $this->getContainer()->get('doctrine')->getEntityManager()->getRepository('UniqueCodeBundle:Reservation')->findUnansweredHotelPropositions();
+        /**
+         * @var $reservationService Reservation
+         */
+        $reservationService = $this->getContainer()->get('unique_code.reservation');
+
+        foreach ($unansweredHotelPropositions as $reservation){
+            $reservationService->customerDeclineHotelProposing($reservation);
+        }
+
+        $output->writeln('Command ended.');
+    }
+}
